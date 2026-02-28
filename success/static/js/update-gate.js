47d99f9caf0f67bd91acce9027df22cff09692a2
Brett A McCall <brettmccall@gmail.com>
2026-02-28 09:28:55 -0500

    fixing security vulnerabilities

--- success/static/js/update-gate.js
+++ success/static/js/update-gate.js
@@ -13,7 +13,7 @@
             date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
             expires = "; expires=" + date.toUTCString();
         }
-        document.cookie = name + "=" + (value || "") + expires + "; path=/";
+        document.cookie = name + "=" + (value || "") + expires + "; path=/; Secure; SameSite=Lax";
     }
 
     // Helper to get cookie
@@ -30,7 +30,7 @@
 
     // Helper to clear cookie
     function eraseCookie(name) {
-        document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+        document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT; Secure; SameSite=Lax';
     }
 
     // Logic
